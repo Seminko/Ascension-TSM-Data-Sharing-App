9bc28372b490b22458f111c72c861d20d8b2f3bb
Seminko <hanyzeman@centrum.cz>
2024-11-14 18:31:54 +0100

    log_exception_message_and_quit after msg fix
    when an inline msg was being written and an exception occured, the log_exception_message_and_quit function would not be writted from the start of the line but rather directly after message, this fixes it

--- main.py
+++ main.py
@@ -309,4 +309,5 @@ if __name__ == "__main__":
     try:
         main()
     except Exception:
+        generic_helper.clear_message(msg)
         generic_helper.log_exception_message_and_quit(max_version)
