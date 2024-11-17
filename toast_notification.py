6386ba719543040ad383099e2773aa398ca234cf
Seminko <hanyzeman@centrum.cz>
2024-11-17 21:06:55 +0100

    toast notification update
    Updated toast_notification as a preparation for auto updating. Also updated create_generic_notification to be able to take a new optional parameter urgent=False which will change sound effect and urgent scenario.

--- toast_notification.py
+++ toast_notification.py
@@ -9,13 +9,18 @@ import winrt.windows.data.xml.dom as dom
 
 # %% FUNCTIONS
 
-def create_generic_notification(title, desc):
+def create_generic_notification(title, desc, urgent=False):
     # Create the toast notifier
-    notifier = ToastNotificationManager.create_toast_notifier('Ascension TSM Data Sharing App')
+    notifier = ToastNotificationManager.create_toast_notifier(APP_NAME)
+    scenario = ""
+    ms_winsoundevent = "Notification.Default"
+    if urgent:
+        scenario = " scenario='urgent'"
+        ms_winsoundevent = "Notification.Looping.Call10"
     
     tString = f"""
-    <toast duration='long'>
-        <audio src='ms-winsoundevent:Notification.Default' loop='false' silent='false'/>
+    <toast duration='long'{scenario}>
+        <audio src='ms-winsoundevent:{ms_winsoundevent}' loop='false' silent='false'/>
         <visual>
             <binding template='ToastText02'>
                 <text id='1'>{title}</text>
@@ -40,13 +45,11 @@ def create_update_notification(mandatory=False):
     # Define the title, description, and URL
     if mandatory:
         title = "⚠️ MANDATORY UPDATE ⚠️"
-        desc = "App WILL NOT run until you update!\nClick the button below, GitHub releases page will open in your default browser."
-        url = "https://github.com/Seminko/Ascension-TSM-Data-Sharing-App/releases"
+        desc = "App WILL NOT run until you update!\nThe app is now paused and is waiting for your input."
         scenario = " scenario='urgent'"
     else:
         title = "Optional Update Available"
-        desc = "If you decide to update, click the button below, GitHub releases page will open in your default browser."
-        url = "https://github.com/Seminko/Ascension-TSM-Data-Sharing-App/releases"
+        desc = "Despite the update being optional, it would be better for everyone if you updated.\nThe app is now paused and is waiting for your input."
         scenario = ""
     
     # Toast XML with clickable description (action)
@@ -59,9 +62,6 @@ def create_update_notification(mandatory=False):
                 <text id='2'>{desc}</text>
             </binding>
         </visual>
-        <actions>
-            <action content="Go to Github releases" activationType="protocol" arguments="{url}" />
-        </actions>
     </toast>
     """
     
@@ -71,30 +71,3 @@ def create_update_notification(mandatory=False):
     
     # Show the toast notification
     notifier.show(ToastNotification(xDoc))
-    
-# def create_upload_reminder_notification(time_played):
-#     # Create the toast notifier
-#     notifier = ToastNotificationManager.create_toast_notifier('Ascension TSM Data Sharing App')
-    
-#     # Define the title, description, and URL
-#     title = "Scan and Upload Reminder"
-#     desc = f"You have been playing for more than {int(time_played/60/60)}&#160;hours.&#10;Please consider doing an AH scan and /reload-ing to trigger DB upload.&#10;Thanks! ❤"
-
-#     tString = f"""
-#     <toast duration='long'>
-#         <audio src='ms-winsoundevent:Notification.Looping.Call10' loop='false' silent='true'/>
-#         <visual>
-#             <binding template='ToastText02'>
-#                 <text id='1'>{title}</text>
-#                 <text id='2'>{desc}</text>
-#             </binding>
-#         </visual>
-#     </toast>
-#     """
-    
-#     # Load the XML content into the notification
-#     xDoc = dom.XmlDocument()
-#     xDoc.load_xml(tString)
-    
-#     # Show the toast notification
-#     notifier.show(ToastNotification(xDoc))
