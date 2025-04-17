580ebe0338961f19488cb48abf1aa58f5f1b13bc
Seminko <hanyzeman@centrum.cz>
2025-04-17 20:08:37 +0200

    WTF folder required Account sub-dir
    Updated so that it checks there actually is Account folder in the WTF folder. HotDabs had two Acension locations and the WTF folder he selected was not actually being used and hence didn't have an Account folder.

--- get_wtf_folder.py
+++ get_wtf_folder.py
@@ -26,10 +26,10 @@ def get_possible_paths():
     drives = [f"{chr(x)}:\\" for x in range(65, 91) if os.path.exists(f"{chr(x)}:\\")]
     for drive in drives:
         # Add default known locations
-        possible_paths.append(os.path.join(drive, "Program Files", "Ascension Launcher", "resources", "client"))
-        possible_paths.append(os.path.join(drive, "Program Files (x86)", "Ascension Launcher", "resources", "client"))
-        possible_paths.append(os.path.join(drive, "Games", "Ascension Launcher", "resources", "client"))
-        possible_paths.append(os.path.join(drive, "Ascension Launcher", "resources", "client"))
+        possible_paths.append(os.path.join(drive, "Program Files", "Ascension Launcher", "resources", "client", "WTF", "Account"))
+        possible_paths.append(os.path.join(drive, "Program Files (x86)", "Ascension Launcher", "resources", "client", "WTF", "Account"))
+        possible_paths.append(os.path.join(drive, "Games", "Ascension Launcher", "resources", "client", "WTF", "Account"))
+        possible_paths.append(os.path.join(drive, "Ascension Launcher", "resources", "client", "WTF", "Account"))
     # # LINUX IS UNTESTED
     # elif os.type == "Linux":
     #     home_dir = os.path.expanduser("~")
@@ -56,12 +56,16 @@ def get_wtf_folder():
         return wtf_folder
 
     while True:
-        logger.info("Couldn't find WTF folder automatically. Select it yourself")
+        logger.info("Couldn't find WTF folder automatically. Select it yourself.")
+        logger.info("MAKE SURE the WTF folder contains Account sub-directory. If not, it is not the correct folder!")
         folder_selected_path = asyncio.run(select_folder())
         if folder_selected_path.endswith(r"/WTF"):
-            break
+            if os.path.isdir(os.path.join(folder_selected_path, "Account")):
+                break
+            else:
+                logger.warn("There is no Account sub-directory in the WTF folder you selected. Find the correct one and try again")
         else:
-            logger.info("Selected wrong folder. You must select the 'WTF' folder in Ascension directory. Try again")
+            logger.warn("Selected wrong folder. You must select the 'WTF' folder in Ascension directory. Try again")
 
     return folder_selected_path
 
@@ -74,8 +78,8 @@ def find_wtf_folder(starting_paths, target_folder_name="WTF"):
     :return: The full path to the WTF folder if found, otherwise None.
     """
     for starting_path in starting_paths:
-        for root, dirs, files in os.walk(starting_path):
-            if target_folder_name in dirs:
-                return os.path.join(root, target_folder_name)
+        if os.path.isdir(starting_path):
+            "Does ./.. from Account back to WTF folder"
+            return os.path.dirname(starting_path)
 
     return None
